69e8102e3d7d1f781dd03f485c05ec6473624583
Win <synwe@yahoo.com>
2017-08-26 10:59:46 +0000

    Package for pip

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+version = "0.2"
+
+setup(
+    name='goerr',
+    packages=find_packages(),
+    version=version,
+    description='Go style explicit errors handling in Python',
+    author='synw',
+    author_email='synwe@yahoo.com',
+    url='https://github.com/synw/goerr',
+    download_url='https://github.com/synw/goerr/releases/tag/' + version,
+    keywords=['errors', "error_handling"],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+    zip_safe=False
+)
