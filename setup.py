8bea7e3cc301957cf703abe93e59cb395e2cea2c
synw <synwx@protonmail.com>
2022-09-18 14:13:00 +0200

    Set language to python 3.8 in setup

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 version = "0.10.0"
 
@@ -7,6 +11,8 @@ setup(
     packages=find_packages(),
     version=version,
     description="Go style explicit errors handling",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     author="synw",
     author_email="synwe@yahoo.com",
     url="https://github.com/synw/goerr",
@@ -16,7 +22,7 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.8",
     ],
     zip_safe=False,
 )
